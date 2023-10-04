5aeb8296750f825d94e7798678ca58f074697ad0
Javier Prieto <jpojaviprior@gmail.com>
2023-10-04 09:17:22 +0200

    initial second commit

---
+++ EjLoloJavi_Apartado2/src/main/java/salesianostriana/dam/EjLoloJavi_Apartado2/EjLoloJaviApartado2Application.java
@@ -0,0 +1,13 @@
+package salesianostriana.dam.EjLoloJavi_Apartado2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjLoloJaviApartado2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjLoloJaviApartado2Application.class, args);
+	}
+
+}
---
+++ EjLoloJavi_Apartado2/src/test/java/salesianostriana/dam/EjLoloJavi_Apartado2/EjLoloJaviApartado2ApplicationTests.java
@@ -0,0 +1,13 @@
+package salesianostriana.dam.EjLoloJavi_Apartado2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjLoloJaviApartado2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
