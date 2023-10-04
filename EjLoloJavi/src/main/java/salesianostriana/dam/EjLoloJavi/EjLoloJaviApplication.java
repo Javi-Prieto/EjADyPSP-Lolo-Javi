1a35f769909a14051093e51401dfa3a52cff0c98
Javier Prieto <jpojaviprior@gmail.com>
2023-10-04 09:14:15 +0200

    initial commit

---
+++ EjLoloJavi/src/main/java/salesianostriana/dam/EjLoloJavi/EjLoloJaviApplication.java
@@ -0,0 +1,13 @@
+package salesianostriana.dam.EjLoloJavi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjLoloJaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjLoloJaviApplication.class, args);
+	}
+
+}
---
+++ EjLoloJavi/src/test/java/salesianostriana/dam/EjLoloJavi/EjLoloJaviApplicationTests.java
@@ -0,0 +1,13 @@
+package salesianostriana.dam.EjLoloJavi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjLoloJaviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
